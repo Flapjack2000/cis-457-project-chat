751847f4cfe8ff99e57859fbddd9184d45cbf025
Zachary Williams <zacharywilliams080802@gmail.com>
2026-04-07 22:53:41 -0400

    messages get sent to other clients

--- server.py
+++ server.py
@@ -16,11 +16,33 @@ def handle_client(sock):
     # Announce to everyone that the new user has joined
     for key in clients:
         if key != username:
-            message = f"{username} has joined the chat!"
+            message = f"{username} has joined the chat!\n"
             clients[key].send(message.encode())
         else:
-            message = f"Welcome {username}!"
+            message = f"Welcome {username}!\n"
             clients[key].send(message.encode())
+
+    # Continuously pass messages to other clients
+    while True:
+        # Receive data from client
+        message = sock.recv(1024).decode()
+
+        # Handle quit
+        if message == "quit":
+
+            # Quit message
+            for key in clients:
+                if key != username:
+                    message = f"{username} has left the chat!\n"
+                    clients[key].send(message.encode())
+
+            # Escape loop to end thread
+            break
+
+        # Pass along message
+        else:
+            for key in clients:
+                clients[key].send(f"{username}: {message}\n".encode())
 
     # Close socket
     clients[username].close()
@@ -37,4 +59,7 @@ while True:
     except KeyboardInterrupt:
         break
 
+# Close everything
+for key in clients:
+    clients[key].close()
 server_sock.close()
