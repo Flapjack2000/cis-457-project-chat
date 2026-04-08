fe0326a0e425499a4f5630f69b114f02b1380edf
Zachary Williams <zacharywilliams080802@gmail.com>
2026-04-07 20:26:53 -0400

    accept socket, spawn thread, read username

--- server.py
+++ server.py
@@ -8,9 +8,21 @@ server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_sock.bind(('', 5000))
 server_sock.listen()
 
+def handle_client(sock):
+    # Receive username from client and store in clients dict
+    username = sock.recv(1024)
+    clients[username] = sock
+
+    # Close socket
+    clients[username].close()
+
+    # Remove client from dict
+    del clients[username]
+
 while True:
     try:
         sock, addr = server_sock.accept()
+        t = threading.Thread(target=handle_client, args=(sock,))
 
     except KeyboardInterrupt:
         break
